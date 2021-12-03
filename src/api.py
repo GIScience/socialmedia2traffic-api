12ceb856da170da2b3f8d2ce29071c0b85c1e28c
Christina Ludwig <christina.ludwig@uni-heidelberg.de>
2021-12-03 10:11:23 +0100

    implemented csv download

--- src/api.py
+++ src/api.py
@@ -8,9 +8,11 @@ __email__ = "christina.ludwig@uni-heidelberg.de"
 
 import json
 
-from flask import Flask
+from flask import Flask, make_response
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 import pandas as pd
+from io import BytesIO
+from flask import send_file
 
 # todo: https://flask-restful.readthedocs.io/en/latest/extending.html#response-formats
 
@@ -22,6 +24,14 @@ api = Api(app)
 data = pd.DataFrame({"osmId": [1, 2],
                      "speed": [100, 80]})
 
+
+@api.representation('text/csv')
+def output_csv(data, code, headers=None):
+    resp = make_response(data.to_csv(), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+
 class Traffic(Resource):
 
     def get(self, bbox):
@@ -31,11 +41,15 @@ class Traffic(Resource):
         :return:
         """
         bbox_list = [float(x) for x in bbox.split(",")]
+        response_stream = BytesIO(data.to_csv(index=False).encode())
+        return send_file(
+            response_stream,
+            mimetype="text/csv",
+            download_name="traffic_bbox.csv",
+        )
 
-        return data.to_json()
 
-
-api.add_resource(Traffic, "/traffic?<string:bbox>")
+api.add_resource(Traffic, "/traffic/<string:bbox>")
 
 if __name__ == "__main__":
     app.run(debug=True)
