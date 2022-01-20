8c0a5aa9736f5ddbd61f3484e6c8aa90ef55f7bb
Christina Ludwig <christina.ludwig@uni-heidelberg.de>
2022-01-20 11:57:39 +0100

    added python-dotenv

--- src/sm2t/__init__.py
+++ src/sm2t/__init__.py
@@ -7,3 +7,10 @@ __author__ = "Christina Ludwig, GIScience Research Group, Heidelberg University"
 __email__ = "christina.ludwig@uni-heidelberg.de"
 
 from .utils import *
+
+from dotenv import load_dotenv
+load_dotenv("../.env", verbose=True)
+
+import logging.config
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+
