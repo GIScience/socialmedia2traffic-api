4d56faf8a3e28ff5096ae3d41a814b89e12a6f32
Christina Ludwig <christina.ludwig@uni-heidelberg.de>
2022-01-20 10:13:24 +0100

    added API tests

--- src/sm2t/test/test_request.py
+++ src/sm2t/test/test_request.py
@@ -8,10 +8,18 @@ __email__ = "christina.ludwig@uni-heidelberg.de"
 
 import requests
 
+BASE = "http://localhost:5000/"
 
-if __name__ == "__main__":
-    BASE = "http://127.0.0.1:5000/"
 
+def test_request_with_data():
+    """Test if request within region with data returns data"""
     response = requests.get(BASE + "traffic/csv?bbox=13.3472,52.499,13.4117,52.5304")
-    print(response)
-    print(response.content)
+    assert response.status_code == 200
+    assert len(response.text) > 100
+
+
+def test_request_without_data():
+    """Test if request outside region with data returns no data"""
+    response = requests.get(BASE + "traffic/csv?bbox=11.32,50.8,11.35,50.82")
+    assert response.status_code == 200
+    assert len(response.text) < 100
